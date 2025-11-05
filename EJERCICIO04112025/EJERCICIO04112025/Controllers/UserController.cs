1d43390410bd2fa21d55cc5fe80e5249b618bb27
123rafael123 <69690701r@gmail.com>
2025-11-04 21:28:51 -0400

    commit horas 9:28

---
+++ EJERCICIO04112025/EJERCICIO04112025/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using EJERCICIO04112025.models.DTOs;
+using EJERCICIO04112025.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EJERCICIO04112025.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("registrar")]
+        public async Task<IActionResult> Register(RegisterUserDto dbo)
+        {
+            throw new NotImplementedException();
+            try
+            {
+                var user = await _userService.RegisterUserAsync(dbo);
+                return Ok(new {message="Usuario creado ",user.Id, user.UserName});
+
+            }
+            catch (Exception ex) {
+                return BadRequest(new { error = ex.Message });
+        }
+    }
+}
---
+++ EJERCICIO04112025/EJERCICIO04112025/models/DTOs/RegisterUserDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EJERCICIO04112025.models.DTOs
+{
+    public class RegisterUserDto
+    {
+        [Required, MaxLength(100)]
+        public string UserName { get; set; } = string.Empty;
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
