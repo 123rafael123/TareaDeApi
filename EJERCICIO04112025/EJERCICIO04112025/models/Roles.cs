7c1f8a1fd5fa5c4080796cce68db2ec8ea245828
123rafael123 <69690701r@gmail.com>
2025-11-11 21:11:25 -0400

    Trate de hacer los modelos :C

---
+++ EJERCICIO04112025/EJERCICIO04112025/models/Roles.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EJERCICIO04112025.models
+{
+    public class Roles
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+        [Required, EmailAddress]
+        public bool Estado { get; set; }
+        [Required]
+        public string Descripcion { get; set; } = string.Empty;
+        public ICollection<User> Users { get; set; }
+    }
+    }
